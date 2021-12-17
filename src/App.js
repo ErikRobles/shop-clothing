6a4acc293b364dd29a86ebcf7cd6c25c0135b746
ErikRobles <erikjames69@hotmail.com>
2021-12-17 13:33:06 -0600

    added email password sign up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='shop/*' element={<ShopPage />} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // this.setState({ currentUser: user });
+      // createUserProfileDocument(user);
+      // console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='shop/*' element={<ShopPage />} />
+          <Route path='signin/*' element={<SignInAndSignUpPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
